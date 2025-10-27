23b5ce7faa2fbd97852b9e522d8948a22a397ef3
AdwaithKulkarni <adwaithkulkarni03@gmail.com>
2025-10-28 04:42:44 +0530

    ,........

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS Configuration
 app.use(cors({
-   origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+   origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://shophub-e-com.onrender.com'],
    credentials: true,
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
    allowedHeaders: ['Content-Type', 'Authorization']
