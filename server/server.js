61dccffd5c2449f829ac679ae71ee455f12e1e23
AdwaithKulkarni <adwaithkulkarni03@gmail.com>
2025-10-28 18:06:16 +0530

    TYPO Fixed

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
    'http://localhost:5173',
    'http://localhost:3000',
    'http://127.0.0.1:5173',
-   'https://shophub-e-com.netlify.app'
+   'https://shophub-ecomm.netlify.app'
 ];
 
 app.use(cors({
